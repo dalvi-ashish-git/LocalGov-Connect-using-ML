fab63beb228c1514c48e0d14558f0376938a040b
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-08-05 17:30:41 +0530

    Added import statements for newly added packages

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import './assets/ms-bundle.css';
+import { StrictMode } from 'react';
+import './components/md3-components.js';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
