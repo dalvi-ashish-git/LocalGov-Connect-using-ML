ba72be5e2b5dc7aca5512ecbf0dcd8d41be38001
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-08-11 21:34:29 +0530

    Added new component for effective page content display and dynamic page resizing

---
+++ front-end/src/components/PageContentPane.jsx
@@ -0,0 +1,14 @@
+import './PageContentPane.css';
+
+function PageContentPane() {
+  return (
+    <div className="panes">
+      <div className="pane">
+        <div className="scroll-wrapper">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageContentPane;
