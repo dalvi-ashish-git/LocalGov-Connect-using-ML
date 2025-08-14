46815e8e95907c77d872a35659b347cd17a73ba8
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-08-14 22:04:44 +0530

    Removed the previous Bottom Navigation related codes from App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -27,42 +27,13 @@ function App() {
     setDrawerOpen((prev) => !(prev));
   };
 
- /* const [activeTab, setActiveTab] = useState('Home');
-
-  const tabs = [
-    { name: 'Home', icon: '🏠' },
-    { name: 'Discover', icon: '🔍' },
-    { name: 'Post', icon: '➕' },
-    { name: 'Notifications', icon: '🔔' },
-    { name: 'Profile', icon: '👤' }
-  ];*/
-
   return (
-   <>
-    <TopAppBar isDrawerOpen={drawerOpen} onMenuClick={toggleNavigationDrawer} />
-    <ModalScrim isDrawerOpen={drawerOpen} onScrimClick={toggleNavigationDrawer} />
-    <ModalNavigationDrawer isDrawerOpen={drawerOpen} />
-    <PageContentPane />
-    <BottomAppBar />
-   {/* <div className="app-container">
-      <div className="screen-content">
-        <h1>{activeTab}</h1>
-        <p>This is the <strong>{activeTab}</strong> screen.</p>
-      </div>
-
-      <nav className="bottom-nav">
-        {tabs.map((tab) => (
-          <button
-            key={tab.name}
-            className={`nav-button ${activeTab === tab.name ? 'active' : ''}`}
-            onClick={() => setActiveTab(tab.name)}
-          >
-            <span className="icon">{tab.icon}</span>
-            <span className="label">{tab.name}</span>
-          </button>
-        ))}
-      </nav>
-    </div>*/}
+    <>
+      <TopAppBar isDrawerOpen={drawerOpen} onMenuClick={toggleNavigationDrawer} />
+      <ModalScrim isDrawerOpen={drawerOpen} onScrimClick={toggleNavigationDrawer} />
+      <ModalNavigationDrawer isDrawerOpen={drawerOpen} />
+      <PageContentPane />
+      <BottomAppBar />
     </>
   );
 }
