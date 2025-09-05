06777d9b4b6a7b41272adb7a0bc9ebfd4f43bdee
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-09-05 20:15:14 +0530

    Added new React element for routing to different pages in the application

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,9 +4,12 @@ import './assets/ms-bundle.css';
 import { StrictMode } from 'react';
 import './components/md3-components.js';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
