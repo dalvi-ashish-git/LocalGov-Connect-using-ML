5bcd18bea8b9e7ea22fd1304ea96b692962068c8
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-09-05 20:42:48 +0530

    Added the Outlet element from React Router DOM for displaying different pages inside PageContentPane.jsx

--- front-end/src/components/PageContentPane.jsx
+++ front-end/src/components/PageContentPane.jsx
@@ -1,10 +1,16 @@
 import './PageContentPane.css';
+import { Outlet } from 'react-router-dom';
 
 function PageContentPane() {
   return (
     <div className="panes">
       <div className="pane">
         <div className="scroll-wrapper">
+          <div className="content">
+            <main className="page-content">
+              <Outlet />
+            </main>
+          </div>
         </div>
       </div>
     </div>
