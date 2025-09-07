7313b57a2687e3f25997d5b3630d7282847454b9
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-09-07 21:00:19 +0530

    Updated CSS styling for all other components to adjust for the new SideNavigationRail component

--- front-end/src/components/TopAppBar.jsx
+++ front-end/src/components/TopAppBar.jsx
@@ -1,10 +1,14 @@
 import './TopAppBar.css';
+import { useRef, useState } from 'react';
 
 function TopAppBar({isDrawerOpen, onMenuClick}) {
+  const themeAnchorRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="top-app-bar">
       <div className="start">
-        <md-icon-button id="nav-menu" className="menu-button" toggle selected={!isDrawerOpen ? true : false}  onClick={onMenuClick}>
+        <md-icon-button id="nav-menu" className="menu-button" toggle selected={!isDrawerOpen}  onClick={onMenuClick}>
           <md-icon slot="selected">menu</md-icon>
           <md-icon>menu_open</md-icon>
         </md-icon-button>
@@ -17,9 +21,29 @@ function TopAppBar({isDrawerOpen, onMenuClick}) {
         <md-icon-button>
           <md-icon>search</md-icon>
         </md-icon-button>
-        <md-icon-button>
-          <md-icon>account_circle</md-icon>
+        <md-icon-button
+          ref={themeAnchorRef}
+          onClick={() => setMenuOpen(prev => !prev)}
+        >
+          <md-icon>palette</md-icon>
         </md-icon-button>
+
+        <md-menu
+          open={menuOpen}
+          anchor={themeAnchorRef.current}
+          onClose={() => setMenuOpen(false)}
+        >
+        <md-menu-item>
+      <div slot="headline">Apple</div>
+    </md-menu-item>
+    <md-menu-item>
+      <div slot="headline">Banana</div>
+    </md-menu-item>
+    <md-menu-item>
+      <div slot="headline">Cucumber</div>
+    </md-menu-item>
+        </md-menu>
+
       </div>
     </div>
   );
