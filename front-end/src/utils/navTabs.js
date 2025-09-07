5fef0f416a6e18165859037dbac35a5994d8b59e
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-09-07 22:44:45 +0530

    Created utils/ folder to store reusable constants and program functions

---
+++ front-end/src/utils/navTabs.js
@@ -0,0 +1,8 @@
+const tabs = [
+  { to: '/', icon: 'home', label: 'Home' },
+  { to: '/report', icon: 'post_add', label: 'Report' },
+  { to: '/updates', icon: 'update', label: 'Updates' },
+  { to: '/community', icon: 'groups', label: 'Community' },
+];
+
+export default tabs;
