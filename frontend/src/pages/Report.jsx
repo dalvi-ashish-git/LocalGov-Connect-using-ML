b5e8b420c6cc38110925f8f62524eaa829aa554c
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2025-10-22 17:23:35 +0530

    Modified Report.jsx file to support different pages for showing reports list, report form and details for a report

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,36 +1,16 @@
 import './Report.css';
-//import ReportForm from './ReportForm';
+import ReportForm from './ReportForm';
+import ReportList from './ReportList';
+import ReportDetails from './ReportDetails';
+import { Routes, Route } from 'react-router-dom';
 
 function Report() {
   return (
-  <>
-  {/*<ReportForm />
- */}   <div className="report-list-container">
-      <md-fab label="Report Issue" variant="primary">
-        <md-icon slot="icon">add</md-icon>
-      </md-fab>
-      <div className="empty-list-state">
-        <div className="empty-state-header">
-          <md-icon class="empty-state-icon">report</md-icon>
-          <h2>No Reports Yet</h2>
-        </div>
-        <p>You haven’t reported any issues so far.</p>
-      </div>
-      <md-list>
-        <h3>Your Reported Issues</h3>
-{Array.from({ length: 20 }).map((_, index) => (
-        <md-list-item type="button" selected>
-          <md-icon slot="start">image</md-icon>
-          <div slot="overline">Overline</div>
-          <div slot="headline">Headline</div>
-          <div slot="supporting-text">Supporting text</div>
-          <div slot="trailing-supporting-text">Trailing</div>
-          <md-icon slot="end">image</md-icon>
-        </md-list-item>
-))}
-      </md-list>
-    </div>
-    </>
+    <Routes>
+      <Route path="" element={<ReportList />} />
+      <Route path="form" element={<ReportForm />} />
+      <Route path=":id" element={<ReportDetails />} />
+    </Routes>
   );
 }
 
